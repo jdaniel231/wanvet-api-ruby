dfc6dceceb0a5ecf931dce7feb172f960a795ce3
hammer200911 <jdaniel231@gmail.com>
2023-05-04 21:09:45 -0300

    aperfeitoando animal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_211312) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_223927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_211312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "age"
+    t.string "castrated"
+    t.string "species"
+    t.string "sex"
+    t.string "breed"
     t.index ["client_id"], name: "index_animals_on_client_id"
   end
 
